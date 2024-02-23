976fdd321f21eb8794e6abfd62143a0ffcf13442
haeun1 <haeun1120@konkuk.ac.kr>
2024-02-23 10:46:37 +0900

    ♻️ refactor : init.sh의 코드 위치 문제 #13 - code 디렉토리의 위치를 바꾸었습니다. - data 디렉토리의 위치를 바꾸었습니다.

--- init.sh
+++ init.sh
@@ -1,23 +1,21 @@
 # code 다운로드
-if [ -d "../code" ] ; then
+if [ -d "code" ] ; then
     echo "code 디렉토리가 이미 있습니다."
 else
-    cd ..
     mkdir code
     cd code
     wget https://aistages-api-public-prod.s3.amazonaws.com/app/Competitions/000269/data/code.tar.gz
     tar -zxvf code.tar.gz
     rm code.tar.gz
     echo "code 디렉토리에 압축을 풀었습니다."
-    cd ../level2-cv-semanticsegmentation-cv-07
+    cd ..
 fi
 
 # data
-if [ -d "../data" ] ; then
+if [ -d "data" ] ; then
     echo "data 디렉토리가 이미 있습니다."
-elif [ -e "../data.tar.gz" ]; then
+elif [ -e "data.tar.gz" ]; then
     # data 디렉토리 생성
-    cd ..
     mkdir data
     cd data
     # data.tar.gz 압축 해제
